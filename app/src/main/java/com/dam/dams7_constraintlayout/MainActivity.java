7702c2ed2cf9696aefcc14afb42851185c66733b
Ricardo Chavez <rachav999iron@gmail.com>
2020-12-10 17:34:25 -0500

    DAMS7_CONSTRAINTLAYOUT_CHAVEZ

---
+++ app/src/main/java/com/dam/dams7_constraintlayout/MainActivity.java
@@ -0,0 +1,27 @@
+package com.dam.dams7_constraintlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnLogin = findViewById(R.id.btnEntrar);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent( MainActivity.this,NotasActivity.class);
+                startActivity(i);
+            }
+        });
+
+    }
+}
